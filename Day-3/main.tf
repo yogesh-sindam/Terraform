df8b93c24a65bd6c4757663ed93efbc514ad5ecc
Yogesh.sindam <91203370+yogesh-sindam@users.noreply.github.com>
2025-02-06 22:51:40 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,1 +1,11 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "./module"
+  ami_value = "ami-053b0d53c279acc90" # replace this
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+}
