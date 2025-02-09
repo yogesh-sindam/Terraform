f4d3b85a1d495c09a25e8516434987989f5f57b8
Yogesh.sindam <91203370+yogesh-sindam@users.noreply.github.com>
2025-02-09 15:20:21 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "creating-for-remoteandstatelocking" # change this
+    key            = "abhi/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
